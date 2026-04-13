31532889b00ff5c290111543e47b168bcd1c219c
Pedro Mateo Espinel Domínguez <mateodomingueze@gmail.com>
2026-04-13 18:49:14 -0500

    API DOCKER

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -5,7 +5,7 @@ const String _physicalDeviceBaseUrl = 'http://192.168.1.2:8000/api/v1';
 const String _localBaseUrl = 'http://127.0.0.1:8000/api/v1';
 
 final String BASE_URL = defaultTargetPlatform == TargetPlatform.android
-    ? _physicalDeviceBaseUrl // ← Cambia esto cuando uses emulador
+    ? _physicalDeviceBaseUrl
     : _localBaseUrl;
 const TIMEOUT_SECONDS = 30;
 const TOKEN_KEY = 'token';
