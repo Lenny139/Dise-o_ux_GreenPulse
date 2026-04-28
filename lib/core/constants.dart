2ce01156d3f3d6ed5a79df4f43f73e3fe0607cba
Pedro Mateo Espinel Domínguez <mateodomingueze@gmail.com>
2026-04-27 22:02:50 -0500

    Funciona desde azure y va rapidito

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/foundation.dart';
 
 const String _androidEmulatorBaseUrl = 'http://10.0.2.2:8000/api/v1';
-const String _localBaseUrl = 'http://127.0.0.1:8000/api/v1';
+const String _localBaseUrl = 'http://48.211.170.34:8000//api/v1';
 const String _physicalDeviceUrl =
-    'http://192.168.1.12:8000/api/v1'; // ← tu IP real
+    'http://48.211.170.34:8000//api/v1'; // ← tu IP real
 
 final String BASE_URL = kIsWeb
     ? _localBaseUrl
